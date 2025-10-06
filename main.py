b3fe45ffccb9577b1f4234ab05387d26f43e8c95
十一 <shiy84321@gmail.com>
2025-10-06 20:16:11 +0800

    精简代码

--- main.py
+++ main.py
@@ -11,142 +11,43 @@ import random
     "1.0.0",
     "https://github.com/your-repo/astrbot_plugin_doro_today"
 )
-class DoroTodayPlugin(Star):
+class StickerPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
+
+    async def _send_random_image(self, event: AstrMessageEvent, category: str):
+        '''一个通用的发送随机图片的辅助方法'''
+        image_folder = os.path.join(os.path.dirname(__file__), category)
+        if not os.path.exists(image_folder):
+            yield event.plain_result(f"{category}文件夹不存在，请检查插件目录")
+            return
+
+        image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+        if not image_files:
+            yield event.plain_result(f"{category}文件夹中没有图片")
+            return
+
+        random_image_file = random.choice(image_files)
+        image_path = os.path.join(image_folder, random_image_file)
+
+        yield event.chain_result([Image.fromFileSystem(image_path)])
 
     @filter.command("doro", alias={'Doro'})
     async def dorotoday(self, event: AstrMessageEvent):
         '''随机抽取一张doro并发送'''
-        # 获取发送者的ID和昵称
-        sender_id = event.get_sender_id()
-        try:
-            sender_name = event.get_sender_name()
-        except AttributeError:
-            sender_name = str(sender_id)
+        await self._send_random_image(event, "doro")
 
-
-        doro_folder = os.path.join(os.path.dirname(__file__), "doro")
-        if not os.path.exists(doro_folder):
-            yield event.plain_result("doro文件夹不存在，请检查插件目录")
-            return
-        
-        # 获取doro文件夹中的所有图片文件
-        image_files = [f for f in os.listdir(doro_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-        
-        if not image_files:
-            yield event.plain_result("doro文件夹中没有图片")
-            return
-        
-        random_image = random.choice(image_files)
-        image_path = os.path.join(doro_folder, random_image)
-        image_name = os.path.splitext(random_image)[0] 
-        
-        message_chain = [
-            Image.fromFileSystem(image_path)  
-        ]
-        
-        # 发送消息
-        yield event.chain_result(message_chain)
-    
-    @filter.command("capoo", alias={'Capoo','猫猫虫','咖波','西诶批欧欧','🐷🐷虫'})
+    @filter.command("capoo", alias={'Capoo', '猫猫虫', '咖波', '西诶批欧欧', '🐷🐷虫'})
     async def capootoday(self, event: AstrMessageEvent):
         '''随机抽取一张capoo并发送'''
-        # 获取发送者的ID和昵称
-        sender_id = event.get_sender_id()
-        try:
-            sender_name = event.get_sender_name()
-        except AttributeError:
-            sender_name = str(sender_id)
+        await self._send_random_image(event, "capoo")
 
-
-        doro_folder = os.path.join(os.path.dirname(__file__), "capoo")
-        if not os.path.exists(doro_folder):
-            yield event.plain_result("capoo文件夹不存在，请检查插件目录")
-            return
-        
-        # 获取capoo文件夹中的所有图片文件
-        image_files = [f for f in os.listdir(doro_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-        
-        if not image_files:
-            yield event.plain_result("capoo文件夹中没有图片")
-            return
-        
-        random_image = random.choice(image_files)
-        image_path = os.path.join(doro_folder, random_image)
-        image_name = os.path.splitext(random_image)[0] 
-        
-        message_chain = [
-            Image.fromFileSystem(image_path)  
-        ]
-        
-        # 发送消息
-        yield event.chain_result(message_chain)
-        
-    @filter.command("cheshire", alias={'Cheshire','柴郡'})
+    @filter.command("cheshire", alias={'Cheshire', '柴郡'})
     async def cheshiretoday(self, event: AstrMessageEvent):
         '''随机抽取一张cheshire并发送'''
-        # 获取发送者的ID和昵称
-        sender_id = event.get_sender_id()
-        try:
-            sender_name = event.get_sender_name()
-        except AttributeError:
-            sender_name = str(sender_id)
+        await self._send_random_image(event, "cheshire")
 
-
-        doro_folder = os.path.join(os.path.dirname(__file__), "cheshire")
-        if not os.path.exists(doro_folder):
-            yield event.plain_result("cheshire文件夹不存在，请检查插件目录")
-            return
-        
-        # 获取cheshire文件夹中的所有图片文件
-        image_files = [f for f in os.listdir(doro_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-        
-        if not image_files:
-            yield event.plain_result("cheshire文件夹中没有图片")
-            return
-        
-        random_image = random.choice(image_files)
-        image_path = os.path.join(doro_folder, random_image)
-        image_name = os.path.splitext(random_image)[0] 
-        
-        message_chain = [
-            Image.fromFileSystem(image_path)  
-        ]
-        
-        # 发送消息
-        yield event.chain_result(message_chain)
-        
-    @filter.command("chiikawa", alias={'Chiikawa','乌萨奇'})
+    @filter.command("chiikawa", alias={'Chiikawa', '乌萨奇'})
     async def chiikawatoday(self, event: AstrMessageEvent):
         '''随机抽取一张chiikawa并发送'''
-        # 获取发送者的ID和昵称
-        sender_id = event.get_sender_id()
-        try:
-            sender_name = event.get_sender_name()
-        except AttributeError:
-            sender_name = str(sender_id)
-
-
-        doro_folder = os.path.join(os.path.dirname(__file__), "chiikawa")
-        if not os.path.exists(doro_folder):
-            yield event.plain_result("chiikawa文件夹不存在，请检查插件目录")
-            return
-        
-        # 获取chiikawa文件夹中的所有图片文件
-        image_files = [f for f in os.listdir(doro_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-        
-        if not image_files:
-            yield event.plain_result("chiikawa文件夹中没有图片")
-            return
-        
-        random_image = random.choice(image_files)
-        image_path = os.path.join(doro_folder, random_image)
-        image_name = os.path.splitext(random_image)[0] 
-        
-        message_chain = [
-            Image.fromFileSystem(image_path)  
-        ]
-        
-        # 发送消息
-        yield event.chain_result(message_chain)
+        await self._send_random_image(event, "chiikawa")
